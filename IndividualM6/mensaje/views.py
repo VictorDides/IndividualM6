638fbb27e961dbc800ec8cb878423043a2980957
VictorDides <vadides@gmail.com>
2023-07-17 20:36:14 -0400

    primera actualizacion Individual M6

---
+++ IndividualM6/mensaje/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+usuarios = [
+    {
+        'nombre': "usuario1",
+        'edad': "10",
+        'mail':"usuario1@individual.m6"
+    },
+    {
+        'nombre': "usuario2",
+        'edad': "20",
+        'mail':"usuario2@individual.m6"
+    },
+    {
+        'nombre': "usuario3",
+        'edad': "30",
+        'mail':"usuario3@individual.m6"
+    },
+    {
+        'nombre': "usuario4",
+        'edad': "40",
+        'mail':"usuario4@individual.m6"
+    },
+    {
+        'nombre': "usuario5",
+        'edad': "50",
+        'mail':"usuario5@individual.m6"
+    }
+
+]
+def mensaje_bienvenida(request):
+    return HttpResponse("<h1> Bienvenidos a TeLoVendo </h1>")
+
+def home(request):
+    contexto ={
+        'usuarios': usuarios
+    }
+    return render(request,'mensaje/index.html', contexto)
